df8561141d4c2c67b3dbd17c41ee23813644f9b0
unknown <johnmal96@yahoo.com>
2026-02-08 17:02:32 +0100

    update site

--- script.js
+++ script.js
@@ -13,9 +13,25 @@
 
   langButtons.forEach((b) => b.addEventListener('click', () => setLang(b.dataset.langBtn)));
 
-  const saved = (() => {
-    try { return localStorage.getItem('tojemoje_lang'); } catch (_) { return null; }
-  })();
+  setLang('pl');
 
-  setLang(saved === 'en' ? 'en' : 'pl');
+  const gallery = document.querySelector('[data-gallery]');
+  const galleryToggle = document.querySelector('[data-gallery-toggle]');
+  if (gallery && galleryToggle) {
+    const photoCount = gallery.querySelectorAll('.photo').length;
+    if (photoCount <= 6) {
+      galleryToggle.style.display = 'none';
+    } else {
+      gallery.dataset.collapsed = 'true';
+      galleryToggle.dataset.expanded = 'false';
+      galleryToggle.setAttribute('aria-expanded', 'false');
+
+      galleryToggle.addEventListener('click', () => {
+        const isCollapsed = gallery.dataset.collapsed !== 'false';
+        gallery.dataset.collapsed = isCollapsed ? 'false' : 'true';
+        galleryToggle.dataset.expanded = isCollapsed ? 'true' : 'false';
+        galleryToggle.setAttribute('aria-expanded', isCollapsed ? 'true' : 'false');
+      });
+    }
+  }
 })();
